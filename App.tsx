03276d80ec5fd510226efeefff64aac7fb34fdf0
DavidManga254 <mangadavid2@gmail.com>
2023-10-29 14:21:15 +0300

    complete rn paper setup

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppNavigation } from './src/navigation/navigation';
 import { NavigationContainer } from '@react-navigation/native';
+import { PaperProvider } from 'react-native-paper';
 export default function App() {
     return (
-        <NavigationContainer>
-            <AppNavigation />
-        </NavigationContainer>
+        <PaperProvider>
+            <NavigationContainer>
+                <AppNavigation />
+            </NavigationContainer>
+        </PaperProvider>
     );
 }
 
