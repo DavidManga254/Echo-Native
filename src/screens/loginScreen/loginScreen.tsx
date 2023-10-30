7323103c789525447642506e5b1cbd4474b03537
DavidManga254 <mangadavid2@gmail.com>
2023-10-30 15:16:31 +0300

    setup login screen

--- src/screens/loginScreen/loginScreen.tsx
+++ src/screens/loginScreen/loginScreen.tsx
@@ -1,12 +1,17 @@
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Text } from 'react-native-paper';
 import { useState } from 'react';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+
 export function LoginScreen() {
     const [securePassword, setSecurePassword] = useState<boolean>(true);
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.form}>
+                <Text style={styles.textInput} variant="headlineMedium">
+                    Login
+                </Text>
                 <TextInput
                     mode="outlined"
                     label={'email'}
@@ -25,6 +30,7 @@ export function LoginScreen() {
                     }
                     style={styles.textInput}
                 />
+
                 <View style={styles.centerView}>
                     <Text style={styles.text}>
                         Do not have an account? <Text style={styles.nestedText}>signup </Text>
